7ce9c3448cc71f59c1921b2b4bd31a7b52258f50
lilakat <katrinar.w1@gmail.com>
2024-11-19 15:08:28 -0500

    changed to v2 but with different background

--- app/components/navigation/navbar/index.tsx
+++ app/components/navigation/navbar/index.tsx
@@ -6,17 +6,19 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
     const pathname = usePathname()
 
     return (<>
-        <div className="w-full h-16 px-4 sm:px-16 bg-white sticky top-0 flex items-center font-[family-name:var(--font-source-sans-pro)] z-50">
+        <div className="w-full h-14 px-6 md:px-24 lg:px-40 bg-white sticky top-0 flex justify-between items-center tracking-wide font-[family-name:var(--font-dmSans)] z-50">
 
-            <ul className="absolute left-4 sm:left-16">
+            <ul className="flex items-center">
                 <li>
-                    <Link href="/" className="text-xl sm:text-xl text-normal text-emerald-900 font-bold">CURRIE LAB</Link>
+                </li>
+                <li className="">
+                    <Link href="/" className="text-xl sm:text-xl text-zinc-900 font-bold">THE CURRIE LAB</Link>
                 </li>
             </ul>
 
             <button
                 type="button"
-                className="inline-flex items-center lg:hidden absolute right-4"
+                className="inline-flex items-center lg:hidden"
                 onClick={toggle}
             >
                 <svg
@@ -32,26 +34,25 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
                 </svg>
             </button>
 
-            <ul className="hidden lg:flex items-center justify-center mx-auto gap-x-12 text-zinc-900 text-lg font-base uppercase">
+            <ul className="hidden lg:flex text-black text-base font-extralight uppercase">
                 <li>
-                    <Link href="/" className={`link ${pathname === '/' ? 'active' : ''} [&.active]:underline underline-offset-4 hover:text-emerald-600`}>About</Link>
+                    <Link href="/" className={`link ${pathname === '/' ? 'active' : ''} [&.active]:font-normal hover:bg-emerald-700 hover:text-white px-4 pb-2 pt-3`}>About</Link>
                 </li>
                 <li>
-                    <Link href="/people" className={`link ${pathname === '/people' ? 'active' : ''} [&.active]:underline underline-offset-4 hover:text-emerald-600`}>People</Link>
+                    <Link href="/people" className={`link ${pathname === '/people' ? 'active' : ''} [&.active]:font-normal hover:bg-emerald-700 hover:text-white px-4 pb-2 pt-3`}>People</Link>
                 </li>
                 <li>
-                    <Link href="/publications" className={`link ${pathname === '/publications' ? 'active' : ''} [&.active]:underline underline-offset-4 hover:text-emerald-600`}>Publications</Link>
+                    <Link href="/publications" className={`link ${pathname === '/publications' ? 'active' : ''} [&.active]:font-normal hover:bg-emerald-700 hover:text-white px-4 pb-2 pt-3`}>Publications</Link>
                 </li>
                 <li>
-                    <Link href="/gallery" className={`link ${pathname === '/gallery' ? 'active' : ''} [&.active]:underline underline-offset-4 hover:text-emerald-600`}>Gallery</Link>
+                    <Link href="/gallery" className={`link ${pathname === '/gallery' ? 'active' : ''} [&.active]:font-normal hover:bg-emerald-700 hover:text-white px-4 pb-2 pt-3`}>Gallery</Link>
                 </li>
                 <li>
-                    <Link href="/contact" className={`link ${pathname === '/contact' ? 'active' : ''} [&.active]:underline underline-offset-4 hover:text-emerald-600`}>Get in touch</Link>
+                    <Link href="/contact" className={`link ${pathname === '/contact' ? 'active' : ''} [&.active]:font-normal hover:bg-emerald-700 hover:text-white px-4 pb-2 pt-3`}>Get in touch</Link>
                 </li>
             </ul>
         </div>
-    </>
-    );
+    </>);
 };
 
 export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,95 @@
+"use client";
+import { useRef } from "react";
 import Image from "next/image";
 
+
 export default function Home() {
-    return (<>
-        <div className="text-black font-[family-name:var(--font-raleway)] min:h-[95vh] flex flex-col md:justify-center items-center text-center md:pt-16 bg-[#f4f7f6]">
-            <h1 className="text-xl sm:text-4xl pb-4 md:pb-12 px-2 font-bold">Welcome to the Currie Lab!</h1>
-            <p className="sm:text-xl px-2 md:px-44">
-                We seek to understand the mechanisms that govern genome organization, epigenetic gene regulation, and genome stability in health and disease. We combine structural biology, biochemistry, and cellular model systems.
-            </p>
+    const secondImageRef = useRef<HTMLDivElement | null>(null);
 
-            <h1 className="text-xl sm:text-2xl font-bold pt-12"> Research Areas</h1>
-            <p className="sm:text-lg py-4 md:py-6 px-2">Cell Biology, Developmental Biology, Genetics / Genomics, Molecular Biology, Pathology, Proteomics, Structural Biology </p>
+    const scrollToBottomOfSecondImage = () => {
+        if (secondImageRef.current) {
+            const rect = secondImageRef.current.getBoundingClientRect();
+            const scrollOffset = window.scrollY + rect.bottom - window.innerHeight;
+            window.scrollTo({ top: scrollOffset, behavior: "smooth" });
+        }
+    };
 
-            <div className="flex justify-center sm:my-8 lg:my-12 overflow-hidden md:object-contain md:w-full">
-                <Image
-                    src="/images/scene2.png"
-                    alt=""
-                    width="1000"
-                    height="500"
-                    className="brightness-75"
-                />
+    return (
+        <>
+            <div className="text-black font-[family-name:var(--font-raleway)]">
+                <div className="relative w-full h-screen flex justify-center items-center overflow-hidden">
+                    <Image
+                        src="/images/scene2.png"
+                        alt=""
+                        layout="fill"
+                        objectFit="cover"
+                        className="h-full"
+                    />
+                    <div className="absolute inset-0 bg-black bg-opacity-60"></div>
+                    <div className="absolute flex flex-col md:flex-row items-center items-centerspace-x-4">
+                        <div className="relative md:border-r md:pr-10">
+                            <Image
+                                src="/images/UofT-Logo-White.png"
+                                alt="UofT Logo"
+                                width="120"
+                                height="120"
+                                objectFit="contain"
+                            />
+                        </div>
+
+                        <div className="pl-2 md:pl-10 tracking-normal sm:tracking-wider">
+                            <h1 className="text-white text-center leading-tight text-4xl md:text-7xl font-bold uppercase">
+                                The Currie Lab
+                            </h1>
+                            <p className="text-white text-center leading-tight text-[1.256em] md:text-[2.45rem] uppercase">
+                                Cell and Systems Biology
+                            </p>
+                            <p className="text-white text-center leading-tight text-[0.91rem] md:text-[1.75rem] pb-2 uppercase">
+                                University of Toronto Mississauga
+                            </p>
+                            <div className="pl-2 text-center md:text-left">
+                                <button onClick={scrollToBottomOfSecondImage}
+                                    className="inline-block px-6 py-3 mt-4 text-white border-2 border-white border-solid rounded-lg transition hover:bg-white hover:bg-opacity-50 uppercase" >
+                                    about
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div id="content" className="flex items-center justify-center h-[60vh]">
+                    <div className="flex flex-col justify-center text-left px-4 md:w-[50vw]" >
+                        <div className="py-4">
+                            <div className=" border-l-8 border-emerald-700 border-solid pl-4">
+                                <h1 className="text-xl sm:text-4xl font-bold"> Welcome to the Currie Lab! </h1>
+                            </div>
+                            <p className="sm:text-lg pt-4 pb-10">
+                                We seek to understand the mechanisms that govern genome organization, epigenetic gene regulation, and genome stability in health and disease. We combine structural biology, biochemistry, and cellular model systems.
+                            </p>
+
+                            <div className=" border-l-8 border-emerald-700 border-solid pl-4">
+                                <h1 className="text-xl sm:text-2xl font-bold"> Research Areas</h1>
+                            </div>
+                            <p className="sm:text-lg py-2">Cell Biology, Developmental Biology, Genetics / Genomics, Molecular Biology, Pathology, Proteomics, Structural Biology </p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div ref={secondImageRef} className="relative w-full h-[20vh] flex justify-center overflow-hidden">
+                    <Image
+                        src="/images/scene2_background_bar.png"
+                        alt=""
+                        layout="fill"
+                        objectFit="cover"
+                        objectPosition="right"
+                        className="w-full brightness-75"
+                    />
+                </div>
             </div>
-        </div>
-    </>);
+        </>
+    );
 }
